ab41a7daf527313b4adb8a76843bbaced3e470bb
Sakthivel.S_IT19158228 <IT19158228@my.sliit.lk>
2022-10-17 07:55:26 +0530

    Commit as at 17/10/22 - Completed UI design and obtained ID's in java class

--- app/src/main/java/com/example/eadlab/CustomerHomepage.java
+++ app/src/main/java/com/example/eadlab/CustomerHomepage.java
@@ -5,19 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class CustomerHomepage extends AppCompatActivity {
+public class CustomerHomepage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private TextView txtVehNumber, txtVehType, txtFuelType;
+    private Spinner spinnerLocation, spinnerShedName;
+    private EditText edtTxtDate;
     private Button btnCheckFuel;
+
+    private String selectedLocation;
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        adapterView.getItemAtPosition(i);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_homepage);
 
+        //Get all the components IDs
+        txtVehNumber = findViewById(R.id.txt_veh_num);
+        txtVehType = findViewById(R.id.txt_veh_type);
+        txtFuelType = findViewById(R.id.txt_veh_fueltype);
+
+        spinnerLocation = findViewById(R.id.spinner_location);
+        spinnerShedName = findViewById(R.id.spinner_fuelstation);
+
+        edtTxtDate = findViewById(R.id.edittxt_date);
+
         btnCheckFuel = findViewById(R.id.btn_checkfuel);
 
+        //Get the vehicle details based on logged in user id
+
+        //Hide the shed name spinner, date field and button
+        spinnerShedName.setVisibility(View.INVISIBLE);
+        edtTxtDate.setVisibility(View.INVISIBLE);
+        btnCheckFuel.setVisibility(View.INVISIBLE);
+
+        spinnerLocation.setOnItemSelectedListener(this);
+
+        //Button click
         btnCheckFuel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/eadlab/ViewFuelDetails.java
+++ app/src/main/java/com/example/eadlab/ViewFuelDetails.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ViewFuelDetails extends AppCompatActivity {
 
+    private TextView txtView_shedLocat, txtView_shedName, txtView_arrTime, txtView_arrAmount, txtView_remAmount, txtView_vehCount;
+    private Spinner spinner_fuelType;
+    private ImageView imgView_fuelStatus;
     private Button btnEnterQueue;
 
     @Override
@@ -16,7 +23,24 @@ public class ViewFuelDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_fuel_details);
 
+        //Get the ID of all elements
+        txtView_shedLocat = findViewById(R.id.txtview_shedLocat);
+        txtView_shedName = findViewById(R.id.txtview_shedName);
+
+        txtView_arrTime = findViewById(R.id.txtview_arrTime);
+        txtView_arrAmount = findViewById(R.id.txtview_arrAmount);
+        txtView_remAmount = findViewById(R.id.txtview_remAmount);
+        txtView_vehCount = findViewById(R.id.txtview_vehCount);
+
+        spinner_fuelType = findViewById(R.id.spinner_fuelType);
+
+        imgView_fuelStatus = findViewById(R.id.imageView_fuelStatus);
+
         btnEnterQueue = findViewById(R.id.btn_enterQueue);
+
+        //Get the petrol shed details based on selected shed name
+
+        //Get the fuel and queue details for selected fuel type
 
         btnEnterQueue.setOnClickListener(new View.OnClickListener() {
             @Override
