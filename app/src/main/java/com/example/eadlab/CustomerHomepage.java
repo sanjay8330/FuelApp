20114abdf9af69b32d3a728d5d8e8f5101f5ff6c
Sakthivel.S_IT19158228 <IT19158228@my.sliit.lk>
2022-10-17 21:03:55 +0530

    Commit as at 17/10/22 - Completed UI level validations and tested

--- app/src/main/java/com/example/eadlab/CustomerHomepage.java
+++ app/src/main/java/com/example/eadlab/CustomerHomepage.java
@@ -10,24 +10,68 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CustomerHomepage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class CustomerHomepage extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private TextView txtVehNumber, txtVehType, txtFuelType;
     private Spinner spinnerLocation, spinnerShedName;
     private EditText edtTxtDate;
     private Button btnCheckFuel;
 
-    private String selectedLocation;
+    private String selectedLocation, selectedShed;
 
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        adapterView.getItemAtPosition(i);
+        switch (adapterView.getId()){
+            case R.id.spinner_location:
+                if(!adapterView.getItemAtPosition(i).toString().equals("Select a location")){
+                    //Toast.makeText(this, "Selected Location is : "+adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show();
+                    selectedLocation = adapterView.getItemAtPosition(i).toString();
+                    this.showUIElements();
+                }else{
+                    selectedLocation = "";
+                    this.hideUIElements();
+                }
+                break;
+            case R.id.spinner_fuelstation:
+                if(!adapterView.getItemAtPosition(i).toString().equals("Select a Fuel Station")){
+                    selectedShed = adapterView.getItemAtPosition(i).toString();
+                }else{
+                    selectedShed = "";
+                }
+                break;
+                //Toast.makeText(this, "Selected Shed Name is"+adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            default:
+                break;
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_checkfuel:
+                if(!selectedLocation.isEmpty() && !selectedShed.isEmpty() && !edtTxtDate.getText().toString().isEmpty()){
+                    Intent intent = new Intent(CustomerHomepage.this, ViewFuelDetails.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this, "Please fill the required fields to continue!"+selectedShed, Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -48,28 +92,40 @@ public class CustomerHomepage extends AppCompatActivity implements AdapterView.O
 
         btnCheckFuel = findViewById(R.id.btn_checkfuel);
 
+        edtTxtDate.setText(this.getCurrentDate());
+
         //Get the vehicle details based on logged in user id
 
         //Hide the shed name spinner, date field and button
+        this.hideUIElements();
+
+        spinnerLocation.setOnItemSelectedListener(this);
+        spinnerShedName.setOnItemSelectedListener(this);
+
+        //Button click
+        btnCheckFuel.setOnClickListener(this);
+    }
+
+    public void showUIElements(){
+        spinnerShedName.setVisibility(View.VISIBLE);
+        edtTxtDate.setVisibility(View.VISIBLE);
+        btnCheckFuel.setVisibility(View.VISIBLE);
+    }
+
+    public void hideUIElements(){
         spinnerShedName.setVisibility(View.INVISIBLE);
         edtTxtDate.setVisibility(View.INVISIBLE);
         btnCheckFuel.setVisibility(View.INVISIBLE);
-
-        spinnerLocation.setOnItemSelectedListener(this);
-
-        if(spinnerLocation.isSelected()){
-            spinnerShedName.setVisibility(View.VISIBLE);
-            edtTxtDate.setVisibility(View.VISIBLE);
-            btnCheckFuel.setVisibility(View.VISIBLE);
-        }
-
-        //Button click
-        btnCheckFuel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CustomerHomepage.this, ViewFuelDetails.class);
-                startActivity(intent);
-            }
-        });
     }
+
+    public String getCurrentDate(){
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+
+        return formattedDate;
+    }
+
 }
--- app/src/main/java/com/example/eadlab/ViewFuelDetails.java
+++ app/src/main/java/com/example/eadlab/ViewFuelDetails.java
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ViewFuelDetails extends AppCompatActivity {
+public class ViewFuelDetails extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private TextView txtView_shedLocat, txtView_shedName, txtView_arrTime, txtView_arrAmount, txtView_remAmount, txtView_vehCount;
     private Spinner spinner_fuelType;
-    private ImageView imgView_fuelStatus;
+    private ImageView imgView_fuelStatus, imgView_fuelQueue;
     private Button btnEnterQueue;
+    private TextView label_arrTime, label_arrLitres, label_remLitres, label_vehCount;
+
+    String selectFuelType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +43,91 @@ public class ViewFuelDetails extends AppCompatActivity {
 
         btnEnterQueue = findViewById(R.id.btn_enterQueue);
 
+        //Default labels & images
+        label_arrTime = findViewById(R.id.label_fueldetail1);
+        label_arrLitres = findViewById(R.id.label_fueldetail2);
+        label_remLitres = findViewById(R.id.label_fueldetail3);
+        label_vehCount = findViewById(R.id.label_currentVehCount);
+        imgView_fuelQueue = findViewById(R.id.imageView_fuelQueue);
+
+        this.hideUIElements();
+
+        spinner_fuelType.setOnItemSelectedListener(this);
+
         //Get the petrol shed details based on selected shed name
 
         //Get the fuel and queue details for selected fuel type
 
-        btnEnterQueue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnEnterQueue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_enterQueue:
+                Toast.makeText(this, "Entered to Queue", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(ViewFuelDetails.this, QueueDetails.class);
                 startActivity(intent);
-            }
-        });
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (adapterView.getId()){
+            case R.id.spinner_fuelType:
+                if(!adapterView.getItemAtPosition(i).toString().equals("Select a Fuel Type")){
+                    selectFuelType = adapterView.getItemAtPosition(i).toString();
+                    this.showUIElements();
+                }else{
+                    selectFuelType = "";
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void hideUIElements(){
+        txtView_arrTime.setVisibility(View.INVISIBLE);
+        txtView_arrAmount.setVisibility(View.INVISIBLE);
+        txtView_remAmount.setVisibility(View.INVISIBLE);
+        txtView_vehCount.setVisibility(View.INVISIBLE);
+
+        label_arrTime.setVisibility(View.INVISIBLE);
+        label_arrLitres.setVisibility(View.INVISIBLE);
+        label_remLitres.setVisibility(View.INVISIBLE);
+        label_vehCount.setVisibility(View.INVISIBLE);
+        imgView_fuelQueue.setVisibility(View.INVISIBLE);
+
+        imgView_fuelStatus.setVisibility(View.INVISIBLE);
+        imgView_fuelQueue.setVisibility(View.INVISIBLE);
+
+        btnEnterQueue.setVisibility(View.INVISIBLE);
+    }
+
+    public void showUIElements(){
+        txtView_arrTime.setVisibility(View.VISIBLE);
+        txtView_arrAmount.setVisibility(View.VISIBLE);
+        txtView_remAmount.setVisibility(View.VISIBLE);
+        txtView_vehCount.setVisibility(View.VISIBLE);
+
+        label_arrTime.setVisibility(View.VISIBLE);
+        label_arrLitres.setVisibility(View.VISIBLE);
+        label_remLitres.setVisibility(View.VISIBLE);
+        label_vehCount.setVisibility(View.VISIBLE);
+        imgView_fuelQueue.setVisibility(View.VISIBLE);
+
+        imgView_fuelStatus.setVisibility(View.VISIBLE);
+        imgView_fuelQueue.setVisibility(View.VISIBLE);
+
+        btnEnterQueue.setVisibility(View.VISIBLE);
     }
 }
