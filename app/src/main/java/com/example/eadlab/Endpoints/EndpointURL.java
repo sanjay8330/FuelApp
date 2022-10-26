4bb1be65cdb553fcfed6dfaba5759bebff51d149
Keshawa-Ekanayake <keshawa456@gmail.com>
2022-10-26 07:41:39 +0530

    Commit by keshawa at 26/10/2022

--- app/src/main/java/com/example/eadlab/Endpoints/EndpointURL.java
+++ app/src/main/java/com/example/eadlab/Endpoints/EndpointURL.java
@@ -2,7 +2,8 @@ package com.example.eadlab.Endpoints;
 
 public class EndpointURL {
     //public static final String endpointAddress = "192.168.1.30:5000";//Change the IP address here (Network and settings -> Wifi properties -> Find the IPv4)
-    public static final String endpointAddress = "192.168.8.104:5000";//Change the IP address here (Network and settings -> Wifi properties -> Find the IPv4)
+//    public static final String endpointAddress = "192.168.8.104:5000";//Change the IP address here (Network and settings -> Wifi properties -> Find the IPv4)
+    public static final String endpointAddress = "192.168.1.100:5000";//Change the IP address here (Network and settings -> Wifi properties -> Find the IPv4)
 
 
     public static final String GET_ALL_CUSTOMERS = "http://" + endpointAddress + "/api/Customer";
@@ -20,5 +21,8 @@ public class EndpointURL {
     public static final String ADD_USER = "http://" + endpointAddress + "/api/Users";
     public static final String ADD_CUSTOMER = "http://" + endpointAddress + "/api/Customer";
     public static final String ADD_SHED = "http://" + endpointAddress + "/api/Shed";
+
+    public static final String GET_USER_BY_ID = "http://" + endpointAddress + "/api/Users/";
+    public static final String ADD_FUEL = "http://" + endpointAddress + "/api/Fuel";
 
 }
